b8648733894f57ae12c8876a3a74e6a5644c5403
iVGeek <ianvincent916@gmail.com>
2023-04-23 13:32:43 -0700

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,98 +1,72 @@
 #!/usr/bin/python3
-''' flask web application for task 3
-    0x04. AirBnB clone - Web framework
-'''
-
-from flask import Flask, escape, render_template
+"""
+Return list of available states either from file or DB storage
+"""
 from models import storage
+from flask import Flask, render_template
 from models.state import State
+import models
+
+
+class StateInfo:
+    """
+    Interface that template receives regardless of storage type
+    """
+    def __init__(self, id, name, cities):
+        """
+        State ID and name
+        """
+        self.id = id
+        self.name = name
+        self.cities = cities
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    '''returns a hello message'''
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    '''returns a hello message'''
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    '''returns a test message'''
-    return 'C {}'.format(escape(text).replace('_', ' '))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythontext(text='is cool'):
-    '''returns a test message'''
-    return 'Python {}'.format(escape(text).replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def numberRoute(n):
-    '''returns a test message'''
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def templateRoute(n):
-    '''returns a test template'''
-    return render_template('5-number.html', number=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def template_odd_even_Route(n):
-    '''returns a test template'''
-    return render_template('6-number_odd_or_even.html', number=n)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    '''lists available states'''
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    '''lists available states with there cities'''
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_by_id(id=None):
-    '''list states by there id and there cities or just states'''
-    states = storage.all(State)
-    if id is not None:
-        state_id = 'State.' + str(id)
-        if state_id in states.keys():
-            state = states[state_id]
-            case = 2
-        else:
-            case = 3
-            state = None
-    else:
-        case = 1
-        state = None
-
-    return render_template('9-states.html', state=state,
-                           states=states.values(), case=case)
-
-
 @app.teardown_appcontext
-def teardown_app(exception):
-    '''teardown the app , closes current sqlalchemy session'''
+def close_storage(exception):
+    """
+    Close file/DB storage on app teardown
+    """
     storage.close()
 
-if __name__ == '__main__':
+
+@app.route("/states", strict_slashes=False)
+def display_states():
+    """
+    Displays all states
+    """
+    states = []
+    if models.storage_type == "db":
+        results = storage.all("State").values()
+    else:
+        results = storage.all(State).values()
+    for result in results:
+        new_state = StateInfo(result.id, result.name, result.cities)
+        states.append(new_state)
+    states.sort(key=lambda s: s.name)
+    return render_template("7-states_list.html",
+                           states=states)
+
+
+@app.route("/states/<string:id>", strict_slashes=False)
+def display_state_with_id(id):
+    """
+    Display a state based on id passed
+    """
+    if models.storage_type == "db":
+        results = list(storage.all("State").values())
+    else:
+        results = list(storage.all(State).values())
+    try:
+        result = [s for s in results if s.id == id][0]
+        state = StateInfo(result.id, result.name, result.cities)
+        state.cities.sort(key=lambda c: c.name)
+    except:
+        state = None
+    return render_template("9-states.html",
+                           state=state)
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
