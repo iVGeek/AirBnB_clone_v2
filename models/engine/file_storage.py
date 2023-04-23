3f4d69bf06f3daab0d841ba177b48655b4ca959c
iVGeek <ianvincent916@gmail.com>
2023-04-23 12:57:01 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,66 +7,50 @@ import models
 
 
 class FileStorage:
-    """
-    serializes and deserializes instances to and from JSON file
-    saved into file_path
-
-    """
-
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        # Initialize attributes
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self, cls=None):
-        """
-        returns a dictionary containing every object
-        """
-        if (not cls):
+        # Returns a dictionary of all objects
+        if cls is None:
             return self.__objects
-        result = {}
-        for key in self.__objects.keys():
-            if (key.split(".")[0] == cls.__name__):
-                result.update({key: self.__objects[key]})
-        return result
+        else:
+            objects_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    objects_dict[key] = value
+            return objects_dict
 
     def new(self, obj):
-        """
-        creates a new object and saves it to __objects
-        """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        # Sets in __objects the obj with key <obj class name>.id
+        key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        """
-        update the JSON file to reflect any change in the objects
-        """
-        temp = {}
-        for id, obj in self.__objects.items():
-            temp[id] = obj.to_dict()
-        with open(self.__file_path, "w") as json_file:
-            json.dump(temp, json_file)
+        # Serializes __objects to the JSON file (path: __file_path)
+        objects_dict = {}
+        for key, value in self.__objects.items():
+            objects_dict[key] = value.to_dict()
+
+        with open(self.__file_path, "w") as f:
+            json.dump(objects_dict, f)
 
     def reload(self):
-        """
-        update __objects dict to restore previously created objects
-        """
+        # Deserializes the JSON file to __objects (only if the file exists)
         try:
-            with open(self.__file_path, "r") as json_file:
-                temp = json.load(json_file)
-            for id, dict in temp.items():
-                temp_instance = models.dummy_classes[dict["__class__"]](**dict)
-                self.__objects[id] = temp_instance
-        except:
+            with open(self.__file_path, "r") as f:
+                objects_dict = json.load(f)
+                for key, value in objects_dict.items():
+                    class_name, obj_id = key.split(".")
+                    class_ = classes[class_name]
+                    obj = class_(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
             pass
 
     def close(self):
-        """display our HBNB data
-        """
+        # Calls the reload method for deserializing the JSON file to objects
         self.reload()
 
-    def delete(self, obj=None):
-        """
-            delete obj from __objects if it’s inside - if obj is None,
-            the method should not do anything
-        """
-        if (obj):
-            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
