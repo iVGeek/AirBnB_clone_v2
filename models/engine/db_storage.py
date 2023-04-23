534cbf1574576929e4fe79d08eb89f72b8f1ca62
iVGeek <ianvincent916@gmail.com>
2023-04-23 12:59:10 -0700

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,40 @@
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        # Create a new session and initialize the attributes
+        self.__session = scoped_session(sessionmaker(bind=self.__engine))()
+
+    def all(self, cls=None):
+        # Returns a dictionary of all objects
+        objects_dict = {}
+        if cls:
+            for obj in self.__session.query(classes[cls]).all():
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                objects_dict[key] = obj
+        else:
+            for cls in classes.values():
+                for obj in self.__session.query(cls).all():
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    objects_dict[key] = obj
+        return objects_dict
+
+    def new(self, obj):
+        # Add obj to the current session
+        self.__session.add(obj)
+
+    def save(self):
+        # Commit all changes to the database
+        self.__session.commit()
+
+    def reload(self):
+        # Create all tables in the database and create a new session
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
+
+    def close(self):
 #!/usr/bin/python3
 """DB storage
 """
@@ -15,72 +52,4 @@ HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
 HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
 
 
-class DBStorage:
-    """database storage for mysql conversion
-    """
-    __engine = None
-    __session = None
 
-    def __init__(self):
-        """initializer for DBStorage"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            HBNB_MYSQL_USER,
-            HBNB_MYSQL_PWD,
-            HBNB_MYSQL_HOST,
-            HBNB_MYSQL_DB), pool_pre_ping=True)
-        env = getenv("HBNB_ENV")
-        if (env == "test"):
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        """Query the current session and list all instances of cls
-        """
-        result = {}
-        if cls:
-            for row in self.__session.query(cls).all():
-                key = "{}.{}".format(cls.__name__, row.id)
-                row.to_dict()
-                result.update({key: row})
-        else:
-            for table in models.dummy_tables:
-                cls = models.dummy_tables[table]
-                for row in self.__session.query(cls).all():
-                    key = "{}.{}".format(cls.__name__, row.id)
-                    row.to_dict()
-                    result.update({key: row})
-        return result
-
-    def rollback(self):
-        """rollback changes
-        """
-        self.__session.rollback()
-
-    def new(self, obj):
-        """add object to current session
-        """
-        self.__session.add(obj)
-
-    def save(self):
-        """commit current done work
-        """
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """delete obj from session
-        """
-        if (obj is None):
-            self.__session.delete(obj)
-
-    def reload(self):
-        """reload the session
-        """
-        Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Scope = scoped_session(Session)
-        self.__session = Scope()
-
-    def close(self):
-        """display our HBNB data
-        """
-        self.__session.__class__.close(self.__session)
-        self.reload()
